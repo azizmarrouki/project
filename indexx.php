fad602c1e96923b858b78b39e567bf2d3e3c3ff5
azizmarrouki <75527245+azizmarrouki@users.noreply.github.com>
2020-12-05 12:46:45 +0100

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="a.css">
+    <title>Portfolio</title>
+    <style>
+        body {
+            background:Indigo;
+           color : white;
+        }
+    </style>
+</head>
+<body>
+  
+        
+<nav>
+ Portfolio
+
+  <a href="#p">About</a>|
+  <a href="#am">Projects</a> |
+  <a href="#mm">Contact</a> |
+  
+  
+</nav>
+<br>
+<div id="p">
+<i><centre><h1><element style="color: red;">Hey I'm Your Name Here</element></h1><centre></i>
+<br>
+A software Developer
+<br>
+    </div>
+
+<a href="https://www.w3schools.com/css/">
+<img src="D.png" id="S"></a>
+<a href="https://developer.mozilla.org/fr/docs/Web/JavaScript">
+<img src="x.jpg" id="d"></a>
+<a href="https://getbootstrap.com">
+<img src="v.png" id="V"></a>
+<a href="https://angular.io">
+<img src="H.png" id="F"></a>
+
+<div id="am">
+<P> PROJECT</P>
+<img src="hh.jpg">
+<img src="KK.jpg" id="m">
+<script src="css/a.css"></script>
+    </div>
+
+    <div id="mm">
+        <p>Contact me</p>
+        <img src="nb.png" id="j">
+</body>
+</html>
